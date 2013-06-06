29a0207a94c714f697c4d8088e7a93bf58b761f9
Antoine Mencière <antoine.menciere@gmail.com>
2013-06-07 00:07:19 +0200

    Finished the "A simple game" tutorial

--- Tetris/src/com/me/tetris/Tetris.java
+++ Tetris/src/com/me/tetris/Tetris.java
@@ -1,7 +1,10 @@
 package com.me.tetris;
 
+import java.util.Iterator;
+
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL10;
@@ -11,8 +14,11 @@ import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.TimeUtils;
 
 public class Tetris implements ApplicationListener {
 	//private OrthographicCamera camera;
@@ -29,6 +35,9 @@ public class Tetris implements ApplicationListener {
 	SpriteBatch batch;
 	
 	Rectangle bucket;
+	Array<Rectangle> raindrops;
+	
+	long lastDropTime;
 	
 	@Override
 	public void create() {
@@ -72,6 +81,9 @@ public class Tetris implements ApplicationListener {
 	    bucket.y = 20;
 	    bucket.width = 64;
 	    bucket.height = 64;
+	    
+	    raindrops = new Array<Rectangle>();
+	    spawnRaindrop();
 	}
 
 	@Override
@@ -80,6 +92,12 @@ public class Tetris implements ApplicationListener {
 		batch.dispose();
 		texture.dispose();
 		*/
+		
+		dropImage.dispose();
+	    bucketImage.dispose();
+	    dropSound.dispose();
+	    rainMusic.dispose();
+	    batch.dispose();
 	}
 
 	@Override
@@ -102,6 +120,9 @@ public class Tetris implements ApplicationListener {
 	    batch.setProjectionMatrix(camera.combined);
 	    batch.begin();
 	    batch.draw(bucketImage, bucket.x, bucket.y);
+	    for(Rectangle raindrop: raindrops) {
+	    	batch.draw(dropImage, raindrop.x, raindrop.y);
+	    }
 	    batch.end();
 	    
 	    if(Gdx.input.isTouched()) {
@@ -109,6 +130,24 @@ public class Tetris implements ApplicationListener {
 	        touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
 	        camera.unproject(touchPos);
 	        bucket.x = touchPos.x - 64 / 2;
+	    }
+	    
+	    if(Gdx.input.isKeyPressed(Keys.LEFT)) bucket.x -= 200 * Gdx.graphics.getDeltaTime();
+	    if(Gdx.input.isKeyPressed(Keys.RIGHT)) bucket.x += 200 * Gdx.graphics.getDeltaTime();
+	    if(bucket.x < 0) bucket.x = 0;
+	    if(bucket.x > 800 - 64) bucket.x = 800 - 64;
+	    
+	    if(TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnRaindrop();
+	    
+	    Iterator<Rectangle> iter = raindrops.iterator();
+	    while(iter.hasNext()) {
+	       Rectangle raindrop = iter.next();
+	       raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
+	       if(raindrop.y + 64 < 0) iter.remove();
+	       if(raindrop.overlaps(bucket)) {
+	    	   dropSound.play();
+	           iter.remove();
+	       }
 	    }
 	}
 
@@ -122,5 +161,15 @@ public class Tetris implements ApplicationListener {
 
 	@Override
 	public void resume() {
+	}
+	
+	private void spawnRaindrop() {
+		Rectangle raindrop = new Rectangle();
+	    raindrop.x = MathUtils.random(0, 800-64);
+	    raindrop.y = 480;
+	    raindrop.width = 64;
+	    raindrop.height = 64;
+	    raindrops.add(raindrop);
+	    lastDropTime = TimeUtils.nanoTime();
 	}
 }
